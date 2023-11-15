c226fe5d901eafecff8a0f93f4bc2f8e3ba0b473
melanie gomez vidal <gomezvidal.melanie@gmail.com>
2023-11-15 23:41:17 +0100

    list restaurants rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+ragazzi = {name: "Ragazzi", address: "sur les quais", phone_number: "0546567890", category: "italian"}
+burger_king =  {name: "Burger King", address: "rue Sainte Catherine", phone_number: "0546569623", category: "belgian"}
+iseo = {name: "Iseo", address: "rue de la Chaine", phone_number: "0546565342", category: "japanese"}
+chez_albert =  {name: "Chez Albert", address: "rue Gargoulleau", phone_number: "0546564123", category: "french"}
+le_marche = {name: "Le Marché", address: "rue du Port", phone_number: "0546569234", category: "french" }
+
+[ragazzi, burger_king, iseo, chez_albert, le_marche].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
